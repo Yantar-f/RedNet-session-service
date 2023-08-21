839a1e9c6540faa6c31f2d81b991c96de895ef39
Матвей Шевченко <shevchenko9542@gmail.com>
2023-08-21 20:51:38 +0600

    initial commit

---
+++ session-service/src/main/java/com/rednet/sessionservice/service/SessionService.java
@@ -0,0 +1,15 @@
+package com.rednet.sessionservice.service;
+
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+public interface SessionService {
+    Mono<ServerResponse> createSession(ServerRequest request);
+    Mono<ServerResponse> getSession(ServerRequest request);
+    Mono<ServerResponse> getSessionsByUserID(ServerRequest request);
+    Mono<ServerResponse> refreshSession(ServerRequest request);
+    Mono<ServerResponse> deleteSession(ServerRequest request);
+    Mono<ServerResponse> deleteSessionsByUserID(ServerRequest request);
+
+}
