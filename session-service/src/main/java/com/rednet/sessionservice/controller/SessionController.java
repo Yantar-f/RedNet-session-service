b5b45d47ed11d30373d1be04c0e0c782b12c789e
Матвей Шевченко <shevchenko9542@gmail.com>
2023-08-31 10:46:54 +0600

    enpoints changed

--- session-service/src/main/java/com/rednet/sessionservice/controller/SessionController.java
+++ session-service/src/main/java/com/rednet/sessionservice/controller/SessionController.java
@@ -7,8 +7,11 @@ import com.rednet.sessionservice.payload.request.RefreshSessionRequestBody;
 import com.rednet.sessionservice.payload.response.SimpleResponseBody;
 import com.rednet.sessionservice.service.SessionService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +21,7 @@ import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@RestController
+@RestController()
 @RequestMapping(produces = APPLICATION_JSON_VALUE)
 public class SessionController {
     private final SessionService sessionService;
@@ -27,33 +30,33 @@ public class SessionController {
         this.sessionService = sessionService;
     }
 
-    @PostMapping(path = "/create-session", consumes = APPLICATION_JSON_VALUE)
+    @PostMapping(path = "/sessions", consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<Session> createSession(@RequestBody CreateSessionRequestBody requestBody) {
         return ResponseEntity.ok(sessionService.createSession(requestBody.userID(),requestBody.roles()));
     }
 
-    @GetMapping("/get-session")
-    public ResponseEntity<Session> getSession(@RequestParam("session-id") String sessionID) {
+    @GetMapping("/sessions/{session-id}")
+    public ResponseEntity<Session> getSession(@PathVariable("session-id") String sessionID) {
         return ResponseEntity.ok(sessionService.getSession(sessionID));
     }
 
-    @GetMapping("/get-sessions-by-user-id")
+    @GetMapping("/sessions")
     public ResponseEntity<List<Session>> getSessionsByUserID(@RequestParam("user-id") String userID) {
         return ResponseEntity.ok(sessionService.getSessionsByUserID(userID));
     }
 
-    @PostMapping("/refresh-session")
+    @PutMapping("/sessions")
     public ResponseEntity<Session> refreshSession(@RequestBody RefreshSessionRequestBody requestBody) {
         return ResponseEntity.ok(sessionService.refreshSession(requestBody.refreshToken()));
     }
 
-    @PostMapping(path = "delete-session", consumes = APPLICATION_JSON_VALUE)
+    @PostMapping(path = "session-removing-process", consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<SimpleResponseBody> deleteSession(@RequestBody DeleteSessionRequestBody requestBody) {
         sessionService.deleteSession(requestBody.refreshToken());
         return ResponseEntity.ok(new SimpleResponseBody("session successfully deleted"));
     }
 
-    @PostMapping("/delete-sessions-by-user-id")
+    @DeleteMapping("/sessions")
     public ResponseEntity<SimpleResponseBody> deleteSessionsByUserID(@RequestParam("user-id") String userID) {
         sessionService.deleteSessionsByUserID(userID);
         return ResponseEntity.ok(new SimpleResponseBody("sessions successfully deleted"));
