dbea7a76fa834489e674c30f102da5690c5661ec
Матвей Шевченко <shevchenko9542@gmail.com>
2023-08-31 23:58:26 +0600

    added simple service discovery client configuration and updated securoty config

--- session-service/src/main/java/com/rednet/sessionservice/SessionServiceApplication.java
+++ session-service/src/main/java/com/rednet/sessionservice/SessionServiceApplication.java
@@ -2,8 +2,12 @@ package com.rednet.sessionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class SessionServiceApplication {
 
     public static void main(String[] args) {
--- session-service/src/main/java/com/rednet/sessionservice/config/SecurityConfig.java
+++ session-service/src/main/java/com/rednet/sessionservice/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
@@ -36,11 +38,12 @@ public class SecurityConfig {
         return httpSecurity
             .csrf(CsrfConfigurer::disable)
             .cors(CorsConfigurer::disable)
+            .httpBasic(HttpBasicConfigurer::disable)
+            .formLogin(FormLoginConfigurer::disable)
             .sessionManagement(session -> session
                 .sessionCreationPolicy(STATELESS))
             .authorizeHttpRequests(auth -> auth
                     .anyRequest().permitAll())
-                    //.anyRequest().authenticated())
             .exceptionHandling(exHandle -> exHandle
                 .accessDeniedHandler(accessDeniedHandler)
                 .authenticationEntryPoint(authenticationEntryPoint))
