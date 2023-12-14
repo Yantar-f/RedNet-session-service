fabebf4be1c0dcfacf6300b48efefedb3f1bb504
yantar <shevchenko9542@gmail.com>
2023-12-14 13:15:39 +0600

    minor changes

--- session-service/src/main/java/com/rednet/sessionservice/service/SessionService.java
+++ session-service/src/main/java/com/rednet/sessionservice/service/SessionService.java
@@ -5,10 +5,10 @@ import com.rednet.sessionservice.entity.Session;
 import java.util.List;
 
 public interface SessionService {
-    Session createSession(String userID, String[] roles);
-    Session getSession(String sessionID);
-    List<Session> getSessionsByUserID(String userID);
-    Session refreshSession(String refreshToken);
-    void deleteSession(String refreshToken);
-    void deleteSessionsByUserID(String userID);
+    Session         createSession           (String userID, String[] roles);
+    Session         getSession              (String sessionID);
+    List<Session>   getSessionsByUserID     (String userID);
+    Session         refreshSession          (String refreshToken);
+    void            deleteSession           (String refreshToken);
+    void            deleteSessionsByUserID  (String userID);
 }
